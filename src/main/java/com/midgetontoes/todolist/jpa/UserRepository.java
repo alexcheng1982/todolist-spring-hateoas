8de7e73ab72dfc944f631a0bdef408db6cd35602
Fu Cheng <alexcheng1982@gmail.com>
2014-11-25 23:11:43 +1300

    add basic security

--- src/main/java/com/midgetontoes/todolist/jpa/UserRepository.java
+++ src/main/java/com/midgetontoes/todolist/jpa/UserRepository.java
@@ -3,5 +3,8 @@ package com.midgetontoes.todolist.jpa;
 import com.midgetontoes.todolist.model.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
